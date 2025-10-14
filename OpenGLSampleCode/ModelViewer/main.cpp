97454de2cfc3a86c567bc4b4e92937ed9742128c
JeanneDeA <Ioannahadjiandreou@gmail.com>
2025-10-15 00:33:25 +0300

    until plane and propellers

--- OpenGLSampleCode/ModelViewer/main.cpp
+++ OpenGLSampleCode/ModelViewer/main.cpp
@@ -10,39 +10,67 @@
 
 
 // Global Variables
-bool g_gamemode;				// GLUT GameMode ON/OFF
-bool g_fullscreen;				// Fullscreen Mode ON/OFF (When g_gamemode Is OFF)
-bool b_culling = false;			// Culling Enabled is Mandatory for this assignment do not change
+bool b_culling = false;
+
+bool movingPlanetFlag = true;
+float anglePlanet = 0.0f;
+
+bool movingPlaneFlag = false;
+float planeSpeed = 0.7f;
+
+
+//===============================================================================================
+
+bool g_gamemode;				
+bool g_fullscreen;				
+
 float aspect = 1;
 
 const float PI = 3.1415926535897932384626433832795028;
 const float epsilon = 0.001;
+float angle = 0;
 
-//Function Prototypes
 void render(void);
 void initLights(void);
 bool init(void);
-void reshape(int w,int h);
+void reshape(int w, int h);
 void keyboard(unsigned char key, int x, int y);
 void special_keys(int a_keys, int x, int y);
+void positionCamera(void);
 
+void createSphere(float radius = 1.0f, int slices = 60, int stacks = 60, float r = 1.0f, float g = 0.0f, float b = 0.0f, float alpha = 1.0f);
+void createStrechedSphere(float radius = 1.0f, int slices = 60, int stacks = 60, float r = 0.8f, float g = 0.8f, float b = 0.0f, float alpha = 1.0f, float scaleX = 1.0f, float scaleY = 1.0f, float scaleZ = 1.5f);
 
-float angle = 0;
+void createPlanet(float planetRadius = 2.0f, int slices = 60, int stacks = 60, float diskInner = 2.5f, float diskOuter = 3.0f);
+void createMoon(float moonRadius = 0.5f, int slices = 60, int stacks = 60);
+void createSun(float maxRadius = 4.0f, float decrement = 0.8f);
+
+void drawAxis(float length = 3.0f);
 
 
 void initLights(void) {
+    glEnable(GL_LIGHTING);
 
-	glEnable(GL_LIGHT0);	
-	//glEnable(GL_LIGHT1);
-	glEnable(GL_LIGHTING);							   // Enable Lighting
-	//glDisable(GL_LIGHTING);
+    // Enable and configure GL_LIGHT0
+    glEnable(GL_LIGHT0);
 
-	GLfloat lightpos[4] = { 0,0,10,0 };
-	glLightfv(GL_LIGHT1, GL_POSITION, lightpos);
+    // Set light position (directional light from above and in front)
+    GLfloat lightPos[] = { 0.0f, 0.0f, 30.0f, 1.0f }; 
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
+    // Set diffuse and ambient color for the light
+    GLfloat diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+
+    // Optionally, set global ambient light
+    GLfloat globalAmbient[] = { 0.1f, 0.1f, 0.1f, 1.0f };
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmbient);
 }
 
-
-// Our GL Specific Initializations. Returns true On Success, false On Fail.
 bool init(void)
 {
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);             // Pixel Storage Mode To Byte Alignment
@@ -69,21 +97,22 @@ bool init(void)
 	return true;
 }
 
-void positionCamera(){
+float cameraX = 0.0f;
+float cameraY = 0.0f;
+float cameraZ = 30.0f; // Default Z
+float cameraStep = 1.0f;
 
-	glMatrixMode(GL_PROJECTION);     // Select The Projection Matrix
-
-	glLoadIdentity();                // Reset The Projection Matrix
-	gluPerspective(45.0f, aspect, 0.1, 100.0);
-	gluLookAt(0.0f,0.0f,30.0f,0.0f,1.0f,0.0f,0.0f,1.0f,0.0f);
-	//gluLookAt(10.0f,- 10.0f, 60.0f, 10.0f, -10.0f, 0.0f, 1.0f, 1.0f, 0.0f);
-	
-	//camera transformations go here
-	glMatrixMode(GL_MODELVIEW);      // Select The Modelview Matrix
-
+void positionCamera() {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, aspect, 0.1, 100.0);
+    gluLookAt(cameraX, cameraY, cameraZ,   // Camera position
+              0.0f, 0.0f, 0.0f,           // Look at the origin
+              0.0f, 1.0f, 0.0f);          // Up vector
+    glMatrixMode(GL_MODELVIEW);
 }
 
-// Our Reshaping Handler (Required Even In Fullscreen-Only Modes)
+
 void reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
@@ -96,51 +125,194 @@ void reshape(int w, int h)
 	initLights();
 }
 
-// Our Keyboard Handler (Normal Keys)
 void keyboard(unsigned char key, int x, int y)
 {
-	switch (key) {
-		case 27:        // When Escape Is Pressed...
-			exit(0);    // Exit The Program
-			break;      // Ready For Next Case
-		default:
-		break;
-	}
-	glutPostRedisplay();
+    switch (key) {
+        case 27: // Escape
+            exit(0);
+            break;
+        case 'w': // Move camera forward (decrease Z)
+            cameraZ -= cameraStep;
+            break;
+        case 's': // Move camera backward (increase Z)
+            cameraZ += cameraStep;
+            break;
+        case 'a': // Move camera left (decrease X)
+            cameraX -= cameraStep;
+            break;
+        case 'd': // Move camera right (increase X)
+            cameraX += cameraStep;
+            break;
+        case 'q': // Move camera up (increase Y)
+            cameraY += cameraStep;
+            break;
+        case 'e': // Move camera down (decrease Y)
+            cameraY -= cameraStep;
+            break;
+        default:
+            break;
+    }
+    positionCamera();
+    glutPostRedisplay();
 }
 
-// Our Keyboard Handler For Special Keys (Like Arrow Keys And Function Keys)
 void special_keys(int a_keys, int x, int y)
 {
+    switch (a_keys) {
+        case GLUT_KEY_UP:
+            cameraY += cameraStep;
+            break;
+        case GLUT_KEY_DOWN:
+            cameraY -= cameraStep;
+            break;
+        case GLUT_KEY_LEFT:
+            cameraX -= cameraStep;
+            break;
+        case GLUT_KEY_RIGHT:
+            cameraX += cameraStep;
+            break;
+        case GLUT_KEY_F1:
+            if (!g_gamemode) {
+                g_fullscreen = !g_fullscreen;
+                if (g_fullscreen) glutFullScreen();
+                else glutReshapeWindow(500, 500);
+            }
+            break;
+        default:
+            break;
+    }
+    positionCamera();
+    glutPostRedisplay();
+}
 
-	switch (a_keys) {
-		case GLUT_KEY_F1:
-			// We Can Switch Between Windowed Mode And Fullscreen Mode Only
-			if (!g_gamemode) {
-				g_fullscreen = !g_fullscreen;       // Toggle g_fullscreen Flag
-				if (g_fullscreen) glutFullScreen(); // We Went In Fullscreen Mode
-				else glutReshapeWindow(500, 500);   // We Went In Windowed Mode
-			}
-		break;
-		case GLUT_KEY_UP:
+void drawAxis(float length) {
+	glDisable(GL_LIGHTING); // Draw axes without lighting for clear colors
 
-			break;
-		case GLUT_KEY_DOWN:
+	glLineWidth(2.0f);
+	glBegin(GL_LINES);
+	// X axis - Red
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(length, 0.0f, 0.0f);
 
-			break;
-		case GLUT_KEY_LEFT:
-			break;
-		case GLUT_KEY_RIGHT:
-			break;
-		default:
-			;
-	}
+	// Y axis - Green
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, length, 0.0f);
 
-	glutPostRedisplay();
+	// Z axis - Blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, length);
+	glEnd();
+
+	glLineWidth(1.0f);
+	glEnable(GL_LIGHTING); // Restore lighting
+}
+
+void createSphere(float radius, int slices , int stacks,float r, float g , float b , float alpha)
+{
+	glPushMatrix();
+	glColor4f(r, g, b, alpha);
+	GLUquadric* sphere = gluNewQuadric();
+	gluSphere(sphere, radius, slices, stacks);
+	gluDeleteQuadric(sphere);
+	glPopMatrix();
+}
+
+void createPlanet(float planetRadius , int slices , int stacks , float diskInner, float diskOuter)
+{
+    glPushMatrix();
+
+    // Draw the planet using createSphere
+    createSphere(planetRadius, slices, stacks, 0.8f, 0.8f, 0.0f, 0.8f);
+
+    // Draw the ring
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glColor4f(0.8f, 0.5f, 0.0f, 0.5f);
+    glRotated(70, 1, 0, 0);
+
+    GLUquadric* ring = gluNewQuadric();
+    gluDisk(ring, diskInner, diskOuter, slices, stacks);
+    gluDeleteQuadric(ring);
+    glDisable(GL_BLEND);
+
+    glPopMatrix();
+}
+
+void createMoon(float moonRadius , int slices, int stacks )
+{
+    createSphere(moonRadius, slices, stacks, 0.5f, 0.5f, 0.5f, 1.0f);
+}
+
+void createSun(float maxRadius, float decrement) {
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDepthMask(GL_FALSE); // Disable depth writing
+
+    createSphere(maxRadius, 60, 60, 1.0f, 0.9f, 0.0f, 0.15f);
+    createSphere(maxRadius-decrement, 60, 60, 1.0f, 0.8f, 0.0f, 0.25f);
+    createSphere(maxRadius - (decrement *2), 60, 60, 1.0f, 0.7f, 0.0f, 0.4f);
+    createSphere(maxRadius - (decrement *3), 60, 60, 1.0f, 0.6f, 0.0f, 1.0f);
+
+	glDepthMask(GLU_TRUE); // Enable depth writing
+    glDisable(GL_BLEND);
+}
+
+void createStrechedSphere( float radius, int slices,int stacks,float r,float g,float b,float alpha,float scaleX,float scaleY,float scaleZ) {
+    glPushMatrix();
+    glColor4f(r, g, b, alpha);
+    glScalef(scaleX, scaleY, scaleZ); 
+    GLUquadric* sphere = gluNewQuadric();
+    gluSphere(sphere, radius, slices, stacks);
+    gluDeleteQuadric(sphere);
+    glPopMatrix();
+}
+
+void createPlane() {
+
+    // Main body:
+	glPushMatrix();
+    createStrechedSphere( 0.5f, 60, 60,		0.2f, 0.2f, 1.0f, 1.0f,		1.0f, 3.0f, 1.0f);
+	glPopMatrix();
+
+	// Hood
+	glPushMatrix();
+	glTranslatef(0.5f, 0.0f, 0.0f);
+	createStrechedSphere(0.5f, 60, 60,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 2.0f, 1.2f);
+	glPopMatrix();
+
+	float propellerAngle = anglePlanet * planeSpeed;
+
+	//Propeller 1
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, 1.0f);
+	createSphere(0.3f, 20, 20, 0.5f, 0.5f, 0.5f, 1.0f);
+	glRotatef(propellerAngle, 0.0f, 0.0f, 1.0f);
+	createStrechedSphere(0.3f, 20, 20,		0.5f, 0.5f, 0.5f, 0.8f,		3.0f, 0.5f, 1.0f);
+	glPopMatrix();	
+
+	//Propeller 1
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -1.0f);
+	createSphere(0.3f, 20, 20, 0.5f, 0.5f, 0.5f, 1.0f);
+	glRotatef(propellerAngle, 0.0f, 0.0f, 1.0f);
+	createStrechedSphere(0.3f, 20, 20,		0.5f, 0.5f, 0.5f, 0.8f,		3.0f, 0.5f, 1.0f);
+	glPopMatrix();
+
+	//Propeller Front
+	glPushMatrix();
+	glTranslatef(0.0f, 0.5f *3.0, 0.0f);
+	createSphere(0.3f, 20, 20, 0.5f, 0.5f, 0.5f, 1.0f);
+	glRotatef(propellerAngle, 0.0f, 1.0f, 0.0f);
+	createStrechedSphere(0.3f, 20, 20, 0.5f, 0.5f, 0.5f, 0.8f, 3.0f, 0.5f, 1.0f);
+	glPopMatrix();
+   
 }
 
 
-// Main Function For Bringing It All Together.
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);                           // GLUT Initializtion
@@ -176,9 +348,8 @@ int main(int argc, char** argv)
 // Our Rendering Is Done Here
 void render(void)   
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 
-	// Do we have culling enabled?
 	if (b_culling == true)
 		glEnable(GL_CULL_FACE);
 	else
@@ -186,9 +357,58 @@ void render(void)
 
 	glLoadIdentity();
 
-	glutSolidSphere(5, 20, 20);
 
-    // Swap The Buffers To Make Our Rendering Visible
+#pragma region Planet
+
+	if (movingPlanetFlag) {
+		anglePlanet += 0.1f;
+		glPushMatrix();
+		glRotated(anglePlanet, 0, 1, 0);
+		createPlanet();
+ 	   	glPopMatrix();
+	}
+	else {
+		createPlanet();
+	}
+	
+#pragma endregion
+
+#pragma region Orbit
+	float orbitRadius = 8.0f;
+	float orbitSpeed = 0.5f;
+	float orbitAngle = anglePlanet * orbitSpeed;
+
+	glPushMatrix();
+	glRotated(orbitAngle, 0, 0, 1);
+	glTranslatef(orbitRadius, 0.0f, 0.0f);
+	createMoon();
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotated(orbitAngle +180.0f, 0 , 0, 1);
+	glTranslatef(orbitRadius, 0.0f, 0.0f);
+	createSun();
+	glPopMatrix();
+
+
+#pragma endregion 
+
+#pragma region Plane
+	float planeOrbitRadius = 4.0f;
+	float planeAngle = anglePlanet * planeSpeed;
+
+	glPushMatrix();
+	glRotated(planeAngle, 0, 0, 1);
+	glTranslated(planeOrbitRadius, 0.0f, 0.0f);
+	createPlane();
+	glPopMatrix();
+
+
+#pragma endregion
+
+	//drawAxis(); // Draw axes at the end
+
     glutSwapBuffers();
-	glutPostRedisplay(); //animation
+	glutPostRedisplay(); 
 }
+
